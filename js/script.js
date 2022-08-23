72826bac7f714e01891aeba127368f04d5a28a2f
Anonim <103999938+Meirzhan06@users.noreply.github.com>
2022-08-23 21:54:56 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,7 +14,49 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.slider').slick({
         dots:true,
-        adaptiveHeight:true
+        adaptiveHeight:true,
+        arrows:false,
+        slidesToShow:3,
+        infinite: true,
         
     });
 });
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    infinite: true,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          arrows:false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows:false,
+          infinite: true,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows:false,
+          infinite: true,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
